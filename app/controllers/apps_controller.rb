a55f11d6cb270dda0d68ce186ab674c4f58efd55
conradsze <conradsze@Conrads-MacBook-Pro.local>
2017-06-26 16:16:17 -0700

    can edit apps and audiences name now

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,14 +1,22 @@
 class AppsController < ApplicationController
 	def index
-		@apps = App.all
+		@apps = App.all.order(:name)
 	end
 
 	def create
-		@app = App.new(params.require(:app).permit(:name, :age))
+		@app = App.new(params.require(:app).permit(:name))
 		if @app.save 
 			redirect_to root_path
 		else
 			redirect_to root_path
 		end
+	end
+
+	def update
+		@app = App.find(params[:id])
+		@app.update(params.require(:app).permit(:name))
+		
+		redirect_to apps_path
+		
 	end
 end
--- app/controllers/audiences_controller.rb
+++ app/controllers/audiences_controller.rb
@@ -1,14 +1,22 @@
 class AudiencesController < ApplicationController
 	def index
-		@audiences = Audience.all
+		@audiences = Audience.all.order(:name)
 	end
 
 	def create
-		@audience = Audience.new(params.require(:audience).permit(:name, :age))
+		@audience = Audience.new(params.require(:audience).permit(:name))
 		if @audience.save 
 			redirect_to root_path
 		else
 			redirect_to root_path
 		end
+	end
+
+	def update
+		@audience = Audience.find(params[:id])
+		@audience.update(params.require(:audience).permit(:name))
+		
+		redirect_to audiences_path
+		
 	end
 end
